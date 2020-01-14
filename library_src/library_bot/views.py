650e62d551a9f6e4e3fe55c3999058ce3e9a328b
ASheyshenov <asheyshenov@beeline.kg>
2020-01-14 21:54:03 +0600

    Wrote a view to search for books in the database. Dependencies for bot telegrams are established

--- library_src/library_bot/views.py
+++ library_src/library_bot/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from library_bot import models
+from django.db.models import Q
 
-# Create your views here.
+
+def search_books(request):
+    keyword = request.GET.get("keyword", "")
+    books = models.Book.objects.all()
+    if keyword:
+        books = models.Book.objects.filter(Q(name__contains=keyword.title()) | Q(author__contains=keyword.title()))
+    books_list = [{
+        'id': book.id,
+        'name': book.name,
+        'author': book.author
+    } for book in books]
+
+    answer = {
+        'status': 'SUCCESS',
+        'books': books_list
+    }
+    return JsonResponse(answer)
